d03550f3d382d51fc18b535d31019f12349321d5
Syed Javeed <Syedjaveed.ix@gmail.com>
2023-12-17 18:55:27 +0530

    SavePoint

---
+++ client/src/main.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
+import './index.css'
+import Layout from './Layout.jsx'
+import Contact from './Components/Contact/Contact.jsx'
+
+const Router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<Layout />}>
+      {/* <Route path='' element={<Catalog />} /> */}
+      <Route path='contact' element={<Contact />} />
+    </Route>
+  )
+)
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+     <RouterProvider router={Router} />
+  </React.StrictMode>,
+)
