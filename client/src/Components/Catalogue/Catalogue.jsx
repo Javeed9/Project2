206ed1f3b6b60c9442aa171c18526dd77439fc20
Syed Javeed <Syedjaveed.ix@gmail.com>
2023-12-17 23:09:28 +0530

    Update

--- client/src/Components/Catalogue/Catalogue.jsx
+++ client/src/Components/Catalogue/Catalogue.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import { products } from './catalogueData'
+import './styles.css'
 
 function Catalogue() {
   return (
-    <div>Catalogue</div>
+    <div className="product__list">
+    {products.map((element) => (
+      <div className="product__item" key={element.id}>
+        <img src={element.imageId} alt="" />
+        <h3>{element.name}</h3>
+      </div>
+    ))}
+  </div>
   )
 }
 
-export default Catalogue
+export default Catalogue;
