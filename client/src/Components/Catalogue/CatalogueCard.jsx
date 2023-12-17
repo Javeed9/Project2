edb9d09d5c8a55d2af22adf3717401974d7cb6cf
Syed Javeed <Syedjaveed.ix@gmail.com>
2023-12-18 00:31:58 +0530

    hover effect pending

--- client/src/Components/Catalogue/CatalogueCard.jsx
+++ client/src/Components/Catalogue/CatalogueCard.jsx
@@ -1,12 +1,12 @@
-import React from 'react'
+import React from 'react';
 
-function CatalogueCard({name, imageSrc}) {
+function CatalogueCard({ name, imageSrc }) {
   return (
-    <div className='p-5 bg-slate-300'>
-        <img className='h-80 w-80' src={imageSrc} alt="catalougeImage" />
-        <h2 className='text-center font-bold mt-2'>{name}</h2>
+    <div className='group p-5 bg-slate-300 transform transition duration-300 ease-in-out hover:scale-110 hover:border-2 hover:border-orange-700 '>
+      <img className='h-80 w-80 object-cover group-hover:scale-110' src={imageSrc} alt="catalougeImage" />
+      <h2 className='text-center font-bold mt-2'>{name}</h2>
     </div>
-  )
+  );
 }
 
-export default CatalogueCard
+export default CatalogueCard;
