bd2aad5e3129050adff491e9b673a33a81842267
Syed Javeed <Syedjaveed.ix@gmail.com>
2023-12-19 00:21:19 +0530

    Update hover on catalogue

--- client/src/Components/Catalogue/CatalogueCard.jsx
+++ client/src/Components/Catalogue/CatalogueCard.jsx
@@ -2,9 +2,10 @@ import React from 'react';
 
 function CatalogueCard({ name, imageSrc }) {
   return (
-    <div className='group p-5 bg-slate-300 transform transition duration-300 ease-in-out hover:scale-110 hover:border-2 hover:border-orange-700 '>
-      <img className='h-80 w-80 object-cover group-hover:scale-110' src={imageSrc} alt="catalougeImage" />
-      <h2 className='text-center font-bold mt-2'>{name}</h2>
+    <div className=' box-border p-4 bg-slate-300 transform transition duration-300 ease-in-out hover:scale-110 hover:border-2 hover:border-orange-700 '>
+      <img className='h-80 w-80 object-cover' src={imageSrc} alt="catalougeImage" />
+      <div className='h-2'></div>
+      <h2 className='text-center font-bold'>{name}</h2>
     </div>
   );
 }
