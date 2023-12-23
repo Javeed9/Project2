19c22f2f6da3e0bffebaac8b6ed3df96fa426bc2
Syed Javeed <Syedjaveed.ix@gmail.com>
2023-12-23 12:47:10 +0530

    path modified

--- backend/middleware/multer.js
+++ backend/middleware/multer.js
@@ -1,8 +1,10 @@
 const multer = require('multer');
+const path = require('path');
+const currDir = path.resolve(__dirname, '..');
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-      cb(null, process.env.UPLOAD_PATH)
+      cb(null, path.join(path, process.env.UPLOAD_PATH))
     },
     filename: function (req, file, cb) {
       cb(null, file.originalname)
